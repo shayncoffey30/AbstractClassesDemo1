40992279c746f4859d6f7cb56fd97cedf8a96b98
Shayn Coffey <Shayncoffey30@gmail.com>
2024-12-14 18:59:22 -0500

    excercise completed

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+
+    public abstract class Vehicle
+    {
+
+
+        public int Year { get; set; } = 1;
+        public string Make { get; set; } = "Default Make";
+        public string Model { get; set; } = "Default Mode";
+
+        public abstract void DriveAbstract(); //stubbed out method
+
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is virtually in drive");
+
+        }
+
+
+    }
+
+}
